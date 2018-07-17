6bc00e9a7703dfd9cacd1f5d63b95e9da81bc376
Mohammad Vosoughi <mohammad1367m@gmail.com>
2018-07-17 21:54:34 +0200

    single sensor with Kalman filter implemented

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ def f_k_k_1(od, t):
                       [0, 1, 0       , t        , 0            , 0.5*t*t      ],
                       [0, 0, 1       , 0        , t            , 0            ],
                       [0, 0, 0       , 1        , 0            , t            ],
-                      [0, 0, 1       , 0        , 1            , 0            ],
-                      [0, 0, 0       , 1        , 0            , 1            ]])
+                      [0, 0, 0       , 0        , 1            , 0            ],
+                      [0, 0, 0       , 0        , 0            , 1            ]])
 class kalman_data:
     def __init__(self, mu, covMat):
         self.mu = mu
@@ -51,54 +51,63 @@ def d_k_k_1(sigma, dt):
 class radar:
     def __init__(self, x_position, y_position, arc_error, distance_error, no_data):
         self.x_position = x_position
-        self.y_position = y_position
+        self.y_position =  y_position
         self.arc_error = arc_error
         self.distance_error = distance_error
         self.no_data = no_data
-        self.last_positions = [[], []]
+        self.last_positions = []
 
-    def get_data(self, x, y, t):
-        distance = np.sqrt(np.power(x - self.x_position, 2) + np.power(y - self.y_position, 2)
-                           ) + (2 * np.random.random_sample() - 1) * self.distance_error
-        arc = np.arctan2(y - self.y_position, x - self.x_position) + (2 * np.random.random_sample() - 1) * self.arc_error
+    def next_data(self, x, y):
+        [distance, arc] = cartesian_to_polar(x-self.x_position, y-self.y_position)
+        distance = distance + (2 * np.random.random_sample() - 1) * self.distance_error
+        arc = arc + (2 * np.random.random_sample() - 1) * self.arc_error
         if (not self.no_data) or np.random.randint(2) == 1:
-            data = polar_to_normal(distance, arc)
+            data = polar_to_cartesian(distance, arc)
             x = data[0] + self.x_position
             y = data[1] + self.y_position
-            self.last_positions[0].append(x)
-            self.last_positions[1].append(y)
-            return [distance, arc]
-        return None
+            self.last_positions.append([x , y])
+        else:
+            self.last_positions.append(None)
 
-    def show_point(self, x, y, t):
-        data = self.get_data(x, y, t)
-        ax.plot([self.last_positions[0][-1]],
-                [self.last_positions[1][-1]] , '.', color='yellow')
+    def get_data(self , t):
+        return self.last_positions[t]
 
-        ax.add_patch(Ellipse((self.last_positions[0][-1], self.last_positions[1][-1]), 300, 6*data[0] * self.arc_error, data[1]*180/np.pi))
+    def get_polar_data(self , t):
+        data = self.get_data(t)
+        if data == None:
+            return None
+        return cartesian_to_polar(data[0]-self.x_position, data[1]-self.y_position)
+
+    def show_point(self, t):
+        data = self.get_polar_data(t)
+        ax.plot([self.last_positions[-1][0]],
+                [self.last_positions[-1][1]] , '.', color='yellow')
 
     def show_bahn(self):
-        ax.plot(self.last_positions[0], self.last_positions[1], label='Flugbahn von radar sicht',
-                color='purple', lw=0.5)  # Flugbahn von radar sicht
+        ax.plot(self.last_positions[0], self.last_positions[1], label='Flugbahn von radar sicht',color='purple', lw=0.5)  # Flugbahn von radar sicht
 
+    def show_ellipse(self, t):
+        data = self.get_polar_data(t)
+        ax.add_patch(Ellipse((self.last_positions[-1][0], self.last_positions[-1][1]), 300, 6*data[0] * self.arc_error, data[1]*180/np.pi))
      
 class kalman_filter:
     def __init__(self, object_data, t):
-        self.object_data = [[object_data, d_k_k_1(40, 1), t]]
+        self.object_data = [[object_data, d_k_k_1(16, 1), t]]
 
-    def prediction(self, mu, covMat, sigma ,dt):
+    def prediction(self, xk_1k_1, pk_1k_1, sigma ,dt):
         dkk_1 = d_k_k_1(sigma, dt)
-        fkk_1 = f_k_k_1(mu, dt)
-        xkk_1 = fkk_1.dot(mu)
-        pkk_1 = fkk_1.dot(covMat).dot(np.transpose(fkk_1)) + dkk_1
+        fkk_1 = f_k_k_1(xk_1k_1, dt)
+        xkk_1 = fkk_1.dot(xk_1k_1)
+        pkk_1 = fkk_1.dot(pk_1k_1).dot(np.transpose(fkk_1)) + dkk_1
         return [xkk_1, pkk_1]
+        
 
     def filtering(self, sensorData, k, Hk, Rk,sigma, t):
         muk_1 = self.object_data[k-1][0]
         covMatk_1 = self.object_data[k-1][1]
         tk_1 = self.object_data[k-1][2]
         [mukk_1, covMatkk_1] = self.prediction(muk_1, covMatk_1 ,sigma ,t - tk_1)
-        print("prediction" , mukk_1 - muk_1)
+
         zkk = np.array([sensorData[0],sensorData[1]])
         vkk_1 = zkk - Hk.dot(muk_1)
         skk_1 = Hk.dot(covMatkk_1).dot(np.transpose(Hk)) + Rk
@@ -106,15 +115,11 @@ class kalman_filter:
         xkk = mukk_1 + wkk_1.dot(vkk_1)
         covMatkk = covMatkk_1 - wkk_1.dot(skk_1).dot(np.transpose(wkk_1))
         self.object_data.append([xkk , covMatkk ,  t])
-        # print("covMatk" , covMatkk_1 - covMatk_1)
-        # print("covMatk|k" , covMatkk - covMatkk_1)
     
     def render(self, t):
-        print("t" , t)
-        print(self.object_data[-1][0])
-        for i in range(0,t-1):
-            mu = self.object_data[i][0]
-            ax.plot([mu[0]], [mu[1]], '.')
+        mu = self.object_data[t][0]
+        ax.plot([mu[0]], [mu[1]], '.')
+
     
         
 
@@ -132,11 +137,14 @@ def animate(i):
     ax.clear()
     # ax.set_xlim(-11000, 11000)
     # ax.set_ylim(-11000, 11000)
-    zk = np.array([x[i],y[i]]) #,velocity_x[i], velocity_y[i],accel_x[i],accel_y[i]
+    radar1.next_data(x[i], y[i] )
+    zk = radar1.get_data(i)
+    # zk = np.array([x[i],y[i]]) 
     if i > 0:
-        kal.filtering(zk, i, Hk, Rk, 25, i)
+        kal.filtering(zk, i, Hk, Rk, 10, i)
         kal.render(i)
     show_flugbahn(i)
+    # radar1.show_point(i)
     # show_velocity(i, True)
     # show_accel(i, True)
     # radar1.show_point(x[i], y[i], i)
@@ -171,11 +179,15 @@ def show_accel(i, dim_2d):
                 [y[i], y[i] + 100*accel_y[i]], color='green')
 
 
-def polar_to_normal(r, phi):
+def polar_to_cartesian(r, phi):
     x = np.cos(phi) * r
     y = np.sin(phi) * r
     return [x, y]
 
+def cartesian_to_polar(x , y):
+    r = np.sqrt(np.power(x, 2) + np.power(y, 2))
+    phi = np.arctan2(y, x)
+    return [r , phi]
 
 def N(x, mu, covMat):
     return 1/np.sqrt((2*np.pi)**2 * np.linalg.det(covMat)) * np.exp(-0.5 * ((np.transpose(x - mu)).dot(np.linalg.inv(covMat))).dot(x - mu))
